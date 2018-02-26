255aed713b67b364fd04313002e858dea4fce728
cavalen <cavalen@hotmail.com>
2018-02-26 16:44:37 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xml:lang="en" xmlns="http://www.w3.org/1999/xhtml" lang="en"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<title>Acme Airlines - Mileage Program</title>
+<link rel="stylesheet" type="text/css" href="css/view.css" media="all">
+<link rel="stylesheet" type="text/css" href="css/view_002.css" media="all">
+<link rel="stylesheet" type="text/css" href="css/theme_23.css" media="all">
+<link href="index_files/css.css" rel="stylesheet" type="text/css">
+
+
+</head>
+<body id="main_body" class=" no_guidelines" style="margin-bottom: 50px;">
+	
+	<div id="form_container" class="WarpShadow WLarge WNormal">
+	
+		<h1><a>Mileage</a></h1>
+		<form id="form_10530" class="appnitro top_label" method="post" data-highlightcolor="#ccdced" action="/demo/view.php">
+			<div class="form_description">
+				<h2>Welcome back to ACME Airlines Mileage Program</h2>
+				<p> </p>
+				<p>Welcome to Acme Miles! Acme Airlines' loyalty program specially designed for our travelers. <br><br>With Acme Miles, you will receive the best loyalty benefits Acme Air has to offer - exclusive benefits that will only be available for our members..</p>
+			</div>
+				<table border=0 style="width:100%">
+				<tr>
+				<th>
+					<ul>
+						<li id="li_1" class="simple_name">
+						<span class="description">Acme ID or email <span id="required_1" class="required">*</span></span>
+						<span class="simple_name_1">
+							<input id="element_1_1" name="element_1_1" class="element text medium" maxlength="255" size="50" type="text">
+						</span>
+						</li>		<li id="li_2">
+						<label class="description" for="element_2">Email </label>
+						<div>
+							<input id="element_2" name="element_2" class="element text medium" maxlength="255" type="text"> 
+						</div> 
+						</li>
+							
+									<li id="li_buttons" class="buttons">
+								<input name="form_id" value="10530" type="hidden">
+								
+								<input name="submit_form" value="1" type="hidden">
+								<input name="page_number" value="1" type="hidden">
+								<input id="submit_form" class="button_text" name="submit_form" value="Sign-In" type="submit">
+						</li>
+					</ul>
+				</th>
+				<th>
+					<img src="http://care.air.acmelatam.com/aalogo.png" align="middle">
+				</th>
+				</tr>
+				</table>
+		</form>	
+		<div id="footer">
+			You are being served by <?php echo gethostname(); ?><br> ACME Airlines and GoSmarter ... 2018 
+		</div>
+	</div>
